9f37689af54dac03d62d21c533237742443f7ef8
bloombigsutdiounity@gmail.com <Request@125>
2020-08-11 18:03:49 +0500

    rummycard
    drag and drop is added in this

---
+++ Assets/dropscript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class dropscript : MonoBehaviour , IDropHandler
+{
+
+    public void OnDrop(PointerEventData eventData)
+      {
+       
+        if (eventData.pointerDrag != null)
+        {
+            //CardManager.instance.ondropcard();
+            //eventData.pointerDrag.GetComponent<RectTransform>().anchoredPosition = this.GetComponent<RectTransform>().anchoredPosition;
+            Debug.Log("OnDrop");
+        }
+
+    }
+}
